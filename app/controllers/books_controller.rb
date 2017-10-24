eaf34e3f82fddeffb17051155bc47d885448f9cc
David Forer <david.j.forer@gmail.com>
2017-10-24 20:30:24 +1100

    added pictures, links some css

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,19 +2,27 @@ class BooksController < ApplicationController
     before_action :find_book, only: [:show, :edit, :update, :destroy]
 
     def index
-        @books = Book.all.order("created_at Desc")
-    end
-
-    def new
-        @book = current_user.books.build
+        if params[:category].blank?
+        @books = Book.all.order("created_at DESC")
+        else 
+            @category_id = Category.find_by(name: params[:category]).id
+            @books = Book.where(:category_id => @category_id).order("created_at DESC")
+        end
     end
 
     def show
         @book = Book.find(params[:id])
     end
 
+    def new
+        @book = current_user.books.build
+        @categories = Category.all.map{ |c| [c.name, c.id] }
+    end
+
     def create
         @book = current_user.books.build(book_params)
+        @book.category_id = params[:category_id]
+
         if @book.save
             redirect_to root_path
         else
@@ -22,11 +30,16 @@ class BooksController < ApplicationController
         end
     end
 
+    def edit
+        @categories = Category.all.map{ |c| [c.name, c.id] }
+    end
+
     def update
+        @book.category_id = params[:category_id]
         if @book.update(book_params)
-                redirect_to book_path(@book)
+           redirect_to book_path(@book)
         else
-                render 'edit'  
+           render 'edit'  
         end  
     end
 
@@ -35,12 +48,10 @@ class BooksController < ApplicationController
         redirect_to root_path
     end
 
-    def edit
-    end
 
     private
         def book_params
-            params.require(:book).permit(:title, :description, :author)
+            params.require(:book).permit(:title, :description, :author, :category_id, :book_img)
         end
         def find_book
             @book = Book.find(params[:id])
